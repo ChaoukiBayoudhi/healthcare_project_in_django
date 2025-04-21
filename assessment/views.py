8cf10d0ae9cadfac9cc036b83f7d44b40d47e850
ChaoukiBayoudhi <chaoukibayoudhi@gmail.com>
2025-04-21 11:19:57 +0100

    Fourth Commit

--- assessment/views.py
+++ assessment/views.py
@@ -13,16 +13,20 @@ class PatientViewSet(viewsets.ModelViewSet):
 class DoctorViewSet(viewsets.ModelViewSet):
     queryset=Doctor.objects.all()
     serializer_class=DoctorSerializer
-    @action(methods=['GET'])
+    @action(methods=['get'], detail=False, 
+            url_path='by_speciality/(?P<speciality>[^/.]+)')
     def get_doctors_by_speciality(self,request,speciality):
-        result=Doctor.objects.filter(speciality__iexact=speciality)
+        result=Doctor.objects.filter(specialty__iexact=speciality)
         if not result.exists():
-            return Response(data='There is no doctor hasing the speciality'+speciality,
+            return Response(data='There is no doctor hasing the speciality '+speciality,
                             status=status.HTTP_204_NO_CONTENT)
         serialized_result=DoctorSerializer(result,many=True)
         #or
         #serialized_result=self.get_serializer(result,many=True)
         return Response(serialized_result.data,status=status.HTTP_200_OK)
+        @action(methods=['GET'],detail=False)
+        def get_patients_by_doctor(self,request,doctor_id):
+            pass
 
 
 class MedicalTestViewSet(viewsets.ModelViewSet):
